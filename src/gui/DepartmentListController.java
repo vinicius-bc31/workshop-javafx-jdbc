329fce05b04a51b81013c09baecdf4d78dbe6c87
Vinicius Batagini Costa <vinicius_batagini.97@hotmail.com>
2020-07-31 10:00:46 -0300

    Initializing action as parameter

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -58,7 +58,7 @@ public class DepartmentListController implements Initializable {
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
 	
-	public void UpdateTableView() {
+	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
